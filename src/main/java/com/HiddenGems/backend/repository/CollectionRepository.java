e1313eeb312bfdaa7abd6fc8292ebace177b5ada
dayshaunlyy <dayshaunlyy@gmail.com>
2026-05-05 21:31:57 -0700

    Revamped Collections for feature of saving collections

--- src/main/java/com/HiddenGems/backend/repository/CollectionRepository.java
+++ src/main/java/com/HiddenGems/backend/repository/CollectionRepository.java
@@ -2,9 +2,11 @@ package com.HiddenGems.backend.repository;
 
 import com.HiddenGems.backend.entity.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
+import java.util.UUID;
 
-public interface CollectionRepository extends JpaRepository<Collection, Long> {
+public interface CollectionRepository extends JpaRepository<Collection, UUID> {
 
-    List<Collection> findByUserId(Long userId);
+    List<Collection> findByUserId(UUID userId);
 }
